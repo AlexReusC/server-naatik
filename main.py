c60c5bcbf83c650c2bebcb4bd1b362f49276b336
alexreusc <alexreusc@gmail.com>
2022-10-26 13:20:21 -0500

    Endpoint for receiving csv and running a model #2

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
-from flask import Flask
+from flask import Flask, request
+from joblib import load
+import pandas as pd
 
 app = Flask(__name__)
+prediction_model = load("dummy-model.joblib")
 
-
-@app.route('/')
-def hello():
-	return "hello world"
+@app.route('/', methods=["POST"])
+def run_models():
+	if request.method == "POST" and request.files:
+		df = pd.read_csv(request.files["data"])
+		prediction = prediction_model.predict(df)
+		return prediction.tolist()
