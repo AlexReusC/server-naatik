02d99548b7f04173a524f73755b97fb11a76744b
alexreusc <alexreusc@gmail.com>
2022-11-08 19:40:58 -0600

    Endpoint for creating images of difference churn vs no churn #14

---
+++ differences_images.py
@@ -0,0 +1,76 @@
+
+import pandas as pd
+import os
+import plotly.graph_objects as go
+from plotly.subplots import make_subplots
+
+def get_dataframe_for_plot_nocategoric(variable, churn, nochurn):
+    bill_amount_churn = churn[variable].describe()[["min", "25%", "50%", "75%", "max"]]
+    bill_amount_no_churn = nochurn[variable].describe()[["min", "25%", "50%", "75%", "max"]]
+
+    difference_bill = pd.DataFrame({"churn": bill_amount_churn, "no churn": bill_amount_no_churn}).reset_index()
+    difference_bill.rename(columns={'index':variable}, inplace=True)
+    return difference_bill
+
+def get_dataframe_for_plot_categoric(variable, churn, nochurn):
+    party_gender_cd_churn = churn.groupby(variable)[variable].count()
+    party_gender_cd_nochurn = nochurn.groupby(variable)[variable].count()
+
+    difference_gender = pd.DataFrame({"churn": party_gender_cd_churn, "no churn": party_gender_cd_nochurn}).reset_index().fillna(0)
+    return difference_gender
+
+def make_butterfly_plot(df, variable, ui):
+
+    # create subplots
+    fig = make_subplots(rows=1, cols=2, specs=[[{}, {}]], shared_xaxes=False,
+                        shared_yaxes=True, horizontal_spacing=0)
+
+    fig.append_trace(go.Bar(x=df['churn'],
+                        y=df[variable], 
+                        text=df["churn"].map('{:,.0f}'.format), #Display the numbers with thousands separators in hover-over tooltip 
+                        textposition='inside',
+                        orientation='h', 
+                        width=0.7, 
+                        showlegend=False, 
+                        marker_color='#4472c4'), 
+                        1, 1) # 1,1 represents row 1 column 1 in the plot grid
+
+    fig.append_trace(go.Bar(x=df['no churn'],
+                        y=df[variable], 
+                        text=df["no churn"].map('{:,.0f}'.format),
+                        textposition='inside',
+                        orientation='h', 
+                        width=0.7, 
+                        showlegend=False, 
+                        marker_color='#ed7d31'), 
+                        1, 2) # 1,2 represents row 1 column 2 in the plot grid
+
+    #fig.show()
+    fig.update_xaxes(showticklabels=False,title_text="churn", row=1, col=1, autorange="reversed")
+    fig.update_xaxes(showticklabels=False,title_text="no churn", row=1, col=2)
+
+    fig.update_layout(title_text="Churn vs No churn: "+variable+" ", 
+                    width=800, 
+                    height=400,
+                    title_x=0.9,
+                    xaxis1={'side': 'top'},
+                    xaxis2={'side': 'top'},)
+
+    #fig.show()
+    os.makedirs(f"static/images/{ui}", exist_ok=True)
+    fig.write_image(f"static/images/{ui}/{variable}.png")
+
+
+def create_images(churn, nochurn, ui):
+	# df_complaints = get_dataframe_for_plot_nocategoric('COMPLAINTS', churn, nochurn)
+	df_bill_amount = get_dataframe_for_plot_nocategoric('BILL_AMOUNT', churn, nochurn)
+	df_years_stayed = get_dataframe_for_plot_nocategoric('Years_stayed', churn, nochurn)
+
+	df_party_gender_cd = get_dataframe_for_plot_categoric('PARTY_GENDER_CD', churn, nochurn)
+	# df_pty_profile_sub_type = get_dataframe_for_plot_categoric('PTY_PROFILE_SUB_TYPE', churn, nochurn)
+
+	make_butterfly_plot(df_party_gender_cd, 'PARTY_GENDER_CD', ui)
+	# make_butterfly_plot(df_complaints, 'COMPLAINTS', ui)
+	make_butterfly_plot(df_bill_amount, 'BILL_AMOUNT', ui)
+	# make_butterfly_plot(df_pty_profile_sub_type, 'PTY_PROFILE_SUB_TYPE', ui)
+	make_butterfly_plot(df_years_stayed, 'Years_stayed', ui)
--- main.py
+++ main.py
@@ -1,13 +1,52 @@
-from flask import Flask, request
+import os
+import uuid
+from flask import Flask, request, send_from_directory, jsonify
+from flask_cors import CORS
 from joblib import load
 import pandas as pd
+import json
+
+def get_probability_churn(probabilities):
+	return list(map(lambda x: x[1], probabilities))
 
 app = Flask(__name__)
-prediction_model = load("dummy-model.joblib")
+CORS(app)
+prediction_model = load("classification-model.joblib")
 
 @app.route('/', methods=["POST"])
 def run_models():
 	if request.method == "POST" and request.files:
+		#get parameters
 		df = pd.read_csv(request.files["data"])
-		prediction = prediction_model.predict(df)
-		return prediction.tolist()
+		slides = json.loads(request.form["slides"])
+		threshold1, threshold2, threshold3 = slides["first-slide"], slides["second-slide"], slides["third-slide"]
+		threshold1, threshold2, threshold3 = float(threshold1), float(threshold2), float(threshold3)
+
+		#prediction
+		prediction = prediction_model.predict_proba(df)
+		prediction = get_probability_churn(prediction)
+
+		#create file
+		prediction_dataframe = pd.DataFrame({"Target": prediction})
+		df = pd.concat([df, prediction_dataframe], axis=1)
+		ui = str(uuid.uuid4())
+		os.makedirs("breakdownPredictions", exist_ok=True)
+		df.to_csv(f"breakdownPredictions/{ui}.csv")
+
+		#aggregate little groups
+		group1 = df[df["Target"] < threshold1]
+		group2 = df[(df["Target"] >= threshold1) & (df["Target"] < threshold2)]
+		group3 = df[(df["Target"] >= threshold2) & (df["Target"] < threshold3)]
+		group4 = df[df["Target"] >= threshold3]
+		group1_acc, group2_acc, group3_acc, group4_acc = group1.agg({"BILL_AMOUNT": "sum"}), group2.agg({"BILL_AMOUNT": "sum"}), group3.agg({"BILL_AMOUNT": "sum"}), group4.agg({"BILL_AMOUNT": "sum"})
+
+		return jsonify({"ui": ui, "acc": {"group1": group1_acc.values[0], "group2": group2_acc.values[0], "group3": group3_acc.values[0], "group4": group4_acc.values[0]}}), 200
+
+@app.route('/retrievecsv', methods=["GET"])
+def retrieve_csv():
+	if request.method == "GET" and request.args.get("ui"):
+		csv_dir = "./breakdownPredictions"
+		filename = request.args.get("ui")
+		csv_file = f"{filename}.csv"
+		return send_from_directory(csv_dir, csv_file), 200
+
