f922931ce489f1b0d4d5a34e687c0b898d6dce80
alexreusc <alexreusc@gmail.com>
2022-11-22 20:32:26 -0600

    add big group support

--- main.py
+++ main.py
@@ -27,6 +27,7 @@ def get_thresholds(slides):
 		threshold1, threshold2, threshold3 = slides["first-slide"], slides["second-slide"], slides["third-slide"]
 		threshold1, threshold2, threshold3 = float(threshold1), float(threshold2), float(threshold3)
 		threshold1, threshold2, threshold3 = threshold1 / 100, threshold2 / 100, threshold3 / 100
+		return threshold1, threshold2, threshold3
 
 def split_by_little_groups(df, threshold1, threshold2, threshold3):
 		group1 = df[df["Probabilidad de churn"] < threshold1]
@@ -46,15 +47,15 @@ def get_little_groups_accs(group1, group2, group3, group4):
 		group1_acc, group2_acc, group3_acc, group4_acc = group1.agg({"BILL_AMOUNT/PROBABILITIES": "sum"}), group2.agg({"BILL_AMOUNT/PROBABILITIES": "sum"}), group3.agg({"BILL_AMOUNT/PROBABILITIES": "sum"}), group4.agg({"BILL_AMOUNT/PROBABILITIES": "sum"})
 		return group1_acc, group2_acc, group3_acc, group4_acc
 
-def save_graphs_images(group1, group2, group3, group4, ui):
+def save_graphs_images(group1, group2, group3, group4, ui, i):
 		little_groups_counts = pd.DataFrame(data={"count": [len(group1), len(group2),len(group3.index), len(group4.index)]}, index=["sin churn", "churn bajo", "churn medio", "churn alto"])
 
 		histogram_little_groups = px.bar(x=little_groups_counts.index, y=little_groups_counts["count"])
 		pie_little_groups = px.pie(little_groups_counts, values=little_groups_counts["count"], names=little_groups_counts.index, title="Grupos")
 
 		os.makedirs(f"static/graphs/{ui}", exist_ok=True)
-		histogram_little_groups.write_image(f"static/graphs/{ui}/histogram.png")
-		pie_little_groups.write_image(f"static/graphs/{ui}/pie.png")
+		histogram_little_groups.write_image(f"static/graphs/{ui}/{i}/histogram.png")
+		pie_little_groups.write_image(f"static/graphs/{ui}/{i}/pie.png")
 
 def differences_churn_nochurn(df, threshold1, ui):
 		churn = df[df["Probabilidad de churn"] < threshold1]
@@ -91,9 +92,19 @@ def add_probability_labels(prediction, thr1, thr2, thr3):
 
 	return prediction_dataframe 
 
+def split_by_big_groups(df):
+	big_groups = []
+	unique_groups = df["big_group"].unique()
+	unique_groups.sort()
+	for group_id in unique_groups:
+		tmp_group = df[df["big_group"] == group_id]
+		big_groups.append(tmp_group)
+	return big_groups
+
 def mock_big_groups(df):
 	np.random.seed(42)
-	df["big_group"] = np.random.choince(1, 4, df.shape[0])
+	df["big_group"] = np.random.randint(1, 4, size=len(df))
+	return df
 
 app = Flask(__name__)
 CORS(app)
@@ -127,13 +138,25 @@ def run_models():
 		prediction_dataframe = add_probability_labels(prediction, threshold1, threshold2, threshold3)
 		df = df.join(prediction_dataframe)
 
-		# df = mock_big_groups(df)
-
-		# print("big groups: ", df)
+		df = mock_big_groups(df)
 
 		ui = str(uuid.uuid4())
 		os.makedirs("breakdownPredictions", exist_ok=True)
 		df.to_csv(f"breakdownPredictions/{ui}.csv")
+
+		big_groups = split_by_big_groups(df)
+
+
+		info = []
+		for i, group in enumerate(big_groups):
+			print("big_group", group)
+			little_group1, little_group2, little_group3, little_group4 = split_by_little_groups(group, threshold1, threshold2, threshold3)
+			little_group1, little_group2, little_group3, little_group4 = add_bill_amount(little_group1, little_group2, little_group3, little_group4)
+			little_group1_acc, little_group2_acc, little_group3_acc, little_group4_acc = get_little_groups_accs(little_group1, little_group2, little_group3, little_group4)
+			save_graphs_images(little_group1, little_group2, little_group3, little_group4, i)
+			state, differences = differences_churn_nochurn(group, threshold1, ui)
+			info.append({"i": i, "acc": {"group1": round(little_group1_acc.values[0],2), "group2": round(little_group2_acc.values[0],2), "group3": round(little_group3_acc.values[0],2), "group4": round(little_group4_acc.values[0],2)}, "differences": differences, "state": state})
+
 
 		group1, group2, group3, group4 = split_by_little_groups(df, threshold1, threshold2, threshold3)
 
