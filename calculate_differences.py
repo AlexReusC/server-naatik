ab8a698f408bc9c71d38682f6b98962562a291bf
alexreusc <alexreusc@gmail.com>
2022-11-30 15:39:42 -0600

    made the differences automatic for all cols

--- calculate_differences.py
+++ calculate_differences.py
@@ -8,14 +8,6 @@ def categorical_difference(churn, nochurn, variable):
 	differences = pd.DataFrame({"churn": churn_count_by_class, "no churn": nochurn_count_by_class}).reset_index().fillna(0)
 	max_row, max_val = None, 0
 	for index, row in differences.iterrows():
-		# difference = row["churn"] - row["no churn"]
-		# avg = (row["churn"] + row["no churn"]) / 2
-		# percentage_difference = (difference / avg) * 100
-
-		# print("Differences: ", row["churn"], row["no churn"], percentage_difference)
-		# if abs(percentage_difference) > abs(max_val):
-		# 	max_row, max_val = row, percentage_difference
-
 		if row["churn"] == 0 or row["no churn"] == 0:
 			row["churn"] = row["churn"] + 1
 			row["no churn"] = row["no churn"] + 1
@@ -30,25 +22,16 @@ def categorical_difference(churn, nochurn, variable):
 		difference = round(difference, 2)
 		return f"El grupo con churn tiene {difference} veces de diferencia con respecto al grupo de no churn de {max_row[variable]} en {variable}"
 
-	# percentage_difference = round(percentage_difference, 2)
-	# text = f"El grupo con churn tiene {abs(percentage_difference)}% {'mas' if max_val > 0 else 'menos'} de {max_row[variable]} en {variable} que el grupo sin churn"
-	# return text
-
-
-
 def noncategorical_difference(churn, nochurn, variable):
 	difference = churn[variable].mean() / nochurn[variable].mean()
-	# avg = (churn[variable].mean() + nochurn[variable].mean()) / 2
-
-	# percentage_difference = round((difference / avg) * 100, 2)
 	text = f"El grupo con churn tiene en promedio {round(difference, 2)} veces de diferencia con respecto al grupo con no churn en {variable}"
 	return text
 
 def get_differences(churn, nochurn):
-	bill_amount_text = noncategorical_difference(churn, nochurn, "BILL_AMOUNT")
-	years_stayed_text = noncategorical_difference(churn, nochurn, "Years_stayed")
-
-	party_nationality_text = categorical_difference(churn, nochurn, "PARTY_NATIONALITY")
-	status_text = categorical_difference(churn, nochurn, "STATUS")
-
-	return {"BILL_AMOUNT": bill_amount_text, "Years_stayed": years_stayed_text, "PARTY_NATIONALITY": party_nationality_text, "STATUS": status_text}
+	data = dict()
+	for col_name, col_type in zip(churn.columns, churn.dtypes):
+		if col_type == "object":
+			data[col_name] = categorical_difference(churn, nochurn, col_name)
+		elif col_type == "float64" or col_type == "int64":
+			data[col_name] = noncategorical_difference(churn, nochurn, col_name)
+	return data
